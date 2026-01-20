250a7bb212ca6724f13e9145e04fc7a27e1504fd
angelo-niessen-93 <angeloniessen40@gmail.com>
2026-01-20 09:26:33 +0100

    added dishes

---
+++ script.js
@@ -0,0 +1,170 @@
+let dishes = [
+  {
+    name: "Spaghetti Bolognese",
+    category: "Warme Speisen",
+    price: 12.90,
+    vegetarian: false,
+    spicyLevel: 1
+  },
+  {
+    name: "Penne Arrabbiata",
+    category: "Warme Speisen",
+    price: 11.50,
+    vegetarian: true,
+    spicyLevel: 3
+  },
+  {
+    name: "Hähnchen-Curry",
+    category: "Warme Speisen",
+    price: 13.50,
+    vegetarian: false,
+    spicyLevel: 3
+  },
+  {
+    name: "Rindersteak mit Ofenkartoffeln",
+    category: "Warme Speisen",
+    price: 19.90,
+    vegetarian: false,
+    spicyLevel: 1
+  },
+  {
+    name: "Gemüsepfanne",
+    category: "Warme Speisen",
+    price: 11.20,
+    vegetarian: true,
+    spicyLevel: 1
+  },
+  {
+    name: "Lasagne al Forno",
+    category: "Warme Speisen",
+    price: 13.90,
+    vegetarian: false,
+    spicyLevel: 1
+  },
+  {
+    name: "Risotto mit Pilzen",
+    category: "Warme Speisen",
+    price: 12.50,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Schnitzel Wiener Art",
+    category: "Warme Speisen",
+    price: 15.90,
+    vegetarian: false,
+    spicyLevel: 0
+  },
+  {
+    name: "Tomatensuppe",
+    category: "Vorspeisen",
+    price: 5.90,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Kürbissuppe",
+    category: "Vorspeisen",
+    price: 6.50,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Bruschetta",
+    category: "Vorspeisen",
+    price: 6.50,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Garnelen in Knoblauchöl",
+    category: "Vorspeisen",
+    price: 8.90,
+    vegetarian: false,
+    spicyLevel: 1
+  },
+  {
+    name: "Caprese",
+    category: "Vorspeisen",
+    price: 7.20,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Antipasti Misto",
+    category: "Vorspeisen",
+    price: 9.50,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Caesar Salad",
+    category: "Salate",
+    price: 9.90,
+    vegetarian: false,
+    spicyLevel: 0
+  },
+  {
+    name: "Griechischer Salat",
+    category: "Salate",
+    price: 8.90,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Rucola-Parmesan-Salat",
+    category: "Salate",
+    price: 9.50,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Salat mit gebratenen Hähnchenstreifen",
+    category: "Salate",
+    price: 11.90,
+    vegetarian: false,
+    spicyLevel: 0
+  },
+  {
+    name: "Linsensalat",
+    category: "Salate",
+    price: 9.20,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Schokoladenkuchen",
+    category: "Desserts",
+    price: 4.90,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Tiramisu",
+    category: "Desserts",
+    price: 5.90,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Vanilleeis mit heißen Himbeeren",
+    category: "Desserts",
+    price: 5.50,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Panna Cotta",
+    category: "Desserts",
+    price: 5.20,
+    vegetarian: true,
+    spicyLevel: 0
+  },
+  {
+    name: "Apfelstrudel mit Vanillesauce",
+    category: "Desserts",
+    price: 5.80,
+    vegetarian: true,
+    spicyLevel: 0
+  }
+];

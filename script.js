34126ca04bdbedf30630219c0935d0c8e2c8ef85
angelo-niessen-93 <angeloniessen40@gmail.com>
2026-01-20 14:36:29 +0100

    added new styles

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let dishes = [
     image: "./assets/img/spaghetti-1604836_640.jpg",
     price: 12.99,
     vegetarian: false,
-    spicyLevel: 1
+    spicyLevel: 1,
   },
   {
     name: "Penne Arrabbiata",
@@ -15,7 +15,7 @@ let dishes = [
     image: "./assets/img/spaghetti-1604836_640.jpg",
     price: 11.55,
     vegetarian: true,
-    spicyLevel: 3
+    spicyLevel: 3,
   },
   {
     name: "Hähnchen-Curry",
@@ -23,7 +23,7 @@ let dishes = [
     image: "./assets/img/curry-7249247_640.jpg",
     price: 13.55,
     vegetarian: false,
-    spicyLevel: 3
+    spicyLevel: 3,
   },
   {
     name: "Rindersteak mit Ofenkartoffeln",
@@ -31,7 +31,7 @@ let dishes = [
     image: "./assets/img/steak-4432946_640.jpg",
     price: 19.99,
     vegetarian: false,
-    spicyLevel: 1
+    spicyLevel: 1,
   },
   {
     name: "Gemüsepfanne",
@@ -39,7 +39,7 @@ let dishes = [
     image: "./assets/img/vegetable-pan-8027678_640.jpg",
     price: 11.25,
     vegetarian: true,
-    spicyLevel: 1
+    spicyLevel: 1,
   },
   {
     name: "Lasagne al Forno",
@@ -47,7 +47,7 @@ let dishes = [
     image: "./assets/img/lasagna-1900529_640.jpg",
     price: 13.99,
     vegetarian: false,
-    spicyLevel: 1
+    spicyLevel: 1,
   },
   {
     name: "Risotto mit Pilzen",
@@ -55,127 +55,128 @@ let dishes = [
     image: "./assets/img/rice-4457143_640.jpg",
     price: 12.55,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Schnitzel Wiener Art",
     category: "Warme Speisen",
+    image: "./assets/img/schnitzel-961131_640.jpg",
     price: 15.99,
     vegetarian: false,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Tomatensuppe",
     category: "Vorspeisen",
     price: 5.99,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Kürbissuppe",
     category: "Vorspeisen",
     price: 6.55,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Bruschetta",
     category: "Vorspeisen",
     price: 6.55,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Garnelen in Knoblauchöl",
     category: "Vorspeisen",
     price: 8.99,
     vegetarian: false,
-    spicyLevel: 1
+    spicyLevel: 1,
   },
   {
     name: "Caprese",
     category: "Vorspeisen",
     price: 7.25,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Antipasti Misto",
     category: "Vorspeisen",
     price: 9.55,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Caesar Salad",
     category: "Salate",
     price: 9.99,
     vegetarian: false,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Griechischer Salat",
     category: "Salate",
     price: 8.99,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Rucola-Parmesan-Salat",
     category: "Salate",
     price: 9.55,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Salat mit gebratenen Hähnchenstreifen",
     category: "Salate",
     price: 11.99,
     vegetarian: false,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Linsensalat",
     category: "Salate",
     price: 9.25,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Schokoladenkuchen",
     category: "Desserts",
     price: 4.99,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Tiramisu",
     category: "Desserts",
     price: 5.99,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Vanilleeis mit heißen Himbeeren",
     category: "Desserts",
     price: 5.55,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Panna Cotta",
     category: "Desserts",
     price: 5.25,
     vegetarian: true,
-    spicyLevel: 0
+    spicyLevel: 0,
   },
   {
     name: "Apfelstrudel mit Vanillesauce",
     category: "Desserts",
     price: 5.85,
     vegetarian: true,
-    spicyLevel: 0
-  }
+    spicyLevel: 0,
+  },
 ];
 
 function renderDishes() {
@@ -183,12 +184,14 @@ function renderDishes() {
   dishesList.innerHTML = "";
 
   const categories = ["Warme Speisen", "Salate", "Desserts", "Vorspeisen"];
-  
-  categories.forEach(category => {
-    const dishesInCategory = dishes.filter(dish => dish.category === category);
+
+  categories.forEach((category) => {
+    const dishesInCategory = dishes.filter(
+      (dish) => dish.category === category,
+    );
     if (dishesInCategory.length > 0) {
       dishesList.innerHTML += `<h2 class="category-title">${category}</h2>`;
-      dishesInCategory.forEach(dish => {
+      dishesInCategory.forEach((dish) => {
         dishesList.innerHTML += renderDishesHTML(dish);
       });
     }
@@ -217,7 +220,7 @@ function renderDishesHTML(dish) {
 }
 
 function addToBasket(dishName) {
-  const dish = dishes.find(d => d.name === dishName);
+  const dish = dishes.find((d) => d.name === dishName);
   if (!basket[dishName]) {
     basket[dishName] = { dish: dish, quantity: 1 };
   } else {
@@ -250,7 +253,8 @@ function renderBasket() {
     `;
   }
 
-  document.getElementById("totalPrice").innerText = `Gesamt: ${totalPrice.toFixed(2)}€`;
+  document.getElementById("totalPrice").innerText =
+    `Gesamt: ${totalPrice.toFixed(2)}€`;
 }
 
 function changeQuantity(dishName, delta) {
@@ -264,7 +268,7 @@ function changeQuantity(dishName, delta) {
 }
 
 document.getElementById("checkoutButton").addEventListener("click", () => {
-  if (Object.keys(basket).length === 0) {  
+  if (Object.keys(basket).length === 0) {
   } else {
     basket = {};
     renderBasket();
@@ -272,19 +276,18 @@ document.getElementById("checkoutButton").addEventListener("click", () => {
 });
 
 window.addEventListener("load", () => {
-    const splash = document.getElementById("splash");
-    const mainContent = document.getElementById("main-content");
+  const splash = document.getElementById("splash");
+  const mainContent = document.getElementById("main-content");
 
-    
+  setTimeout(() => {
+    splash.style.transition = "opacity 0.5s";
+    splash.style.opacity = 0;
+
     setTimeout(() => {
-        splash.style.transition = "opacity 0.5s";
-        splash.style.opacity = 0;
-
-        setTimeout(() => {
-            splash.style.display = "none";
-            mainContent.style.display = "block";
-            document.body.style.overflow = "auto"; 
-            renderDishes();
-        }, 500);
-    }, 1500);
+      splash.style.display = "none";
+      mainContent.style.display = "block";
+      document.body.style.overflow = "auto";
+      renderDishes();
+    }, 500);
+  }, 1500);
 });
--- script/burger.js
+++ script/burger.js
@@ -1,9 +1,8 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav');
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
 const aboList = document.getElementById("aboList");
 
-
-burger.addEventListener('click', () => {
-  const open = nav.classList.toggle('active');
-  burger.setAttribute('aria-expanded', open);
+burger.addEventListener("click", () => {
+  const open = nav.classList.toggle("active");
+  burger.setAttribute("aria-expanded", open);
 });
